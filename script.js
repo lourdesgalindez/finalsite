05e1d4c275acf6ec7edcfa968d59dbfd5b0fd33a
lourdesgalindez <54605023+lourdesgalindez@users.noreply.github.com>
2019-12-09 22:39:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.materialboxed');
+   var instances = M.Materialbox.init(elems);
+ });
+
+ document.addEventListener('DOMContentLoaded', function() {
    var elems = document.querySelectorAll('.slider');
    var instances = M.Slider.init(elems);
  });
-
-
-
- // var instance = M.Slider.getInstance(elem);
-
-     /* jQuery Method Calls
-       You can still use the old jQuery plugin method calls.
-       But you won't be able to access instance properties.
-
-       $('.slider').slider('methodName');
-       $('.slider').slider('methodName', paramName);
-     */
-
-
-//   instance.pause();
-//   instance.start();
-//   instance.next();
-//   instance.prev();
-//
-// instance.destroy();
